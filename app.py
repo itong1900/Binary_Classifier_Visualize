90382b0d0877b2ff77327db8a8ff07d891744bc0
itong1900 <71299664+itong1900@users.noreply.github.com>
2022-05-10 22:45:14 -0700

    Refactor session state (#1)
    * Finished refactor data input page
    
    * add legends and add the marginal x dist plot for all in one plot, also make train size in session state
    
    * Finish editting metrics plots, add marginal histograms
    
    * update metrics and curve output layouts
    
    * major updates, fix bugs, add shaded area, tunable threshold, KNN, sessionState of data
    
    * add handcraft Knn, and spiral data configurations

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from debugpy import configure
 import streamlit as st
 
 import numpy as np
+from numpy import pi
 import pandas as pd
 
 import plotly.express as px
+import plotly.graph_objs as go
 
 from libraries.binary_classifier_tool import binary_classifier_tool
 
+from libraries.models import knn
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.utils import class_weight
 
 
@@ -23,75 +28,163 @@ def main():
 
     if mode == "Input Data":
         st.header("Data")
+
+        data_style = st.radio("Data Generation", ('normal', 'spiral'))
+        if data_style == "normal":
         # container = st.container()
         # configure_saved = container.button('Save Config?')
 
-        col1, col2, col3, col4 = st.columns(4)
-        class_A_X1_center = col1.number_input('Classs A, X_1 center', value = 1)
-        class_A_X2_center = col1.number_input('Classs A, X_2 center', value = 1)
-        class_A_X1_sd = col1.number_input('Classs A, X_1 SD', value = 0.5)
-        class_A_X2_sd = col1.number_input('Classs A, X_2 SD', value = 0.5)
+            col1, col2, col3, _ = st.columns(4)
+
+            ## Initialize or set Class 1 configurations
+            col1.write("Class 1 Configurations")
+            st.session_state.class_1_X1_center = col1.number_input(
+                'class 1, X_1 center', 
+                value = st.session_state.class_1_X1_center if 'class_1_X1_center' in st.session_state else 10)
+            st.session_state.class_1_X2_center = col1.number_input(
+                'class 1, X_2 center', 
+                value = st.session_state.class_1_X2_center if 'class_1_X2_center' in st.session_state else 10)
+            st.session_state.class_1_X1_sd = col1.number_input(
+                'class 1, X_1 SD', 
+                value = st.session_state.class_1_X1_sd if 'class_1_X1_sd' in st.session_state else 5)
+            st.session_state.class_1_X2_sd = col1.number_input(
+                'class 1, X_2 SD', 
+                value = st.session_state.class_1_X2_sd if 'class_1_X2_sd' in st.session_state else 5)
+            st.session_state.class_1_size = col1.number_input(
+                'Class 1 size', 
+                value = st.session_state.class_1_size if 'class_1_size' in st.session_state else 10000)
+
+            df1 = pd.DataFrame({"X_1": pd.Series(np.random.normal(st.session_state.class_1_X1_center, st.session_state.class_1_X1_sd, st.session_state.class_1_size) ),
+                                "X_2": pd.Series(np.random.normal(st.session_state.class_1_X2_center, st.session_state.class_1_X2_sd, st.session_state.class_1_size) ),
+                                "Class": pd.Series(["1" for _ in range(st.session_state.class_1_size)])} )
         
-        class_B_X1_center = col2.number_input('Classs B, X_1 center', value = 0)
-        class_B_X2_center = col2.number_input('Classs B, X_2 center', value = 0)
-        class_B_X1_sd = col2.number_input('Classs B, X_1 SD', value = 0.5)
-        class_B_X2_sd = col2.number_input('Classs B, X_2 SD', value = 0.5)
+            ## Initialize or set Class 0 configurations
+            col2.write("Class 0 Configurations")
+            st.session_state.class_0_X1_center = col2.number_input(
+                'Class 0, X_1 center', 
+                value = st.session_state.class_0_X1_center if 'class_0_X1_center' in st.session_state else 0)
+            st.session_state.class_0_X2_center = col2.number_input(
+                'Class 0, X_2 center', 
+                value = st.session_state.class_0_X2_center if 'class_0_X2_center' in st.session_state else 0)
+            st.session_state.class_0_X1_sd = col2.number_input(
+                'Class 0, X_1 SD', 
+                value = st.session_state.class_0_X2_sd if 'class_0_X2_sd' in st.session_state else 3)
+            st.session_state.class_0_X2_sd = col2.number_input(
+                'Class 0, X_2 SD', 
+                value = st.session_state.class_0_X2_sd if 'class_0_X2_sd' in st.session_state else 3)
+            st.session_state.class_0_size = col2.number_input(
+                'Classs 0 size', 
+                value = st.session_state.class_0_size if 'class_0_size' in st.session_state else 1000)
+            
+            df2 = pd.DataFrame({"X_1": pd.Series(np.random.normal(st.session_state.class_0_X1_center, st.session_state.class_0_X1_sd, st.session_state.class_0_size)),
+                                "X_2": pd.Series(np.random.normal(st.session_state.class_0_X2_center, st.session_state.class_0_X2_sd, st.session_state.class_0_size)),
+                                "Class": pd.Series(["0" for _ in range(st.session_state.class_0_size)])})
 
-        class_A_size = col1.number_input('Classs A size', value = 10000)
-        class_B_size = col2.number_input('Classs B size', value = 1000)
+            st.session_state.raw_data = pd.concat([df1, df2]) #df1.append(df2, ignore_index = True)
 
-        st.session_state.X1_class_A = np.random.normal(class_A_X1_center, class_A_X1_sd, class_A_size)
-        st.session_state.X2_class_A = np.random.normal(class_A_X2_center, class_A_X2_sd, class_A_size)
-        st.session_state.X1_class_B = np.random.normal(class_B_X1_center, class_B_X1_sd, class_B_size)
-        st.session_state.X2_class_B = np.random.normal(class_B_X2_center, class_B_X2_sd, class_B_size)
+            fig = px.scatter(st.session_state.raw_data, x="X_1", y="X_2", color="Class", width=600, height=600)
+            fig.add_trace(
+                go.Scatter(
+                    name="axis",
+                    x=[0, 0, None, min(st.session_state.raw_data["X_1"]) , max(st.session_state.raw_data["X_1"]) ],
+                    y=[min(st.session_state.raw_data["X_2"]) , max(st.session_state.raw_data["X_2"]),  None, 0, 0],
+                    mode="lines",
+                    line=go.scatter.Line(color="purple"),
+                    showlegend=False)
+                )
 
+            col3.plotly_chart(fig, use_container_width=False)
+        
+        elif data_style == "spiral":
+            col1, col2, col3, _ = st.columns(4)
 
-        df1 = pd.DataFrame({"X_1": pd.Series(st.session_state.X1_class_A),
-                            "X_2": pd.Series(st.session_state.X2_class_A),
-                            "Class": pd.Series(["1" for _ in range(class_A_size)])})
-        df2 = pd.DataFrame({"X_1": pd.Series(st.session_state.X1_class_B),
-                            "X_2": pd.Series(st.session_state.X2_class_B),
-                            "Class": pd.Series(["0" for _ in range(class_B_size)])})
+            ## Initialize or set Class A configurations
+            col1.write("Class A Configurations")
+            st.session_state.class_A_size = col1.number_input(
+                'Class A size', 
+                value = st.session_state.class_A_size if 'class_A_size' in st.session_state else 1000)
+            st.session_state.class_A_theta_size = col1.number_input(
+                'Class A Spiral Half Cycle', 
+                value = st.session_state.class_A_theta_size if 'class_A_theta_size' in st.session_state else 2)
+            st.session_state.class_A_noise = col1.number_input(
+                'Class A, SD', 
+                value = st.session_state.class_A_noise if 'class_A_noise' in st.session_state else 1)
 
-        st.session_state.raw_data = df1.append(df2, ignore_index = True)
+            theta_1 = np.sqrt(np.random.rand(st.session_state.class_A_size))* st.session_state.class_A_theta_size * pi # np.linspace(0,2*pi,100)
 
-        fig = px.scatter(st.session_state.raw_data, x="X_1", y="X_2", color="Class", symbol="Class", width=600, height=600)
-        col3.plotly_chart(fig, use_container_width=False)
+            r_a = 2*theta_1 + pi
+            data_a = np.array([np.cos(theta_1)*r_a, np.sin(theta_1)*r_a]).T
+            x_a = data_a + st.session_state.class_A_noise *np.random.randn(st.session_state.class_A_size, 2)
 
-        #configure_saved = st.button('Save Config?')
+            df1 = pd.DataFrame({"X_1": pd.Series(x_a[:, 0]),
+                                "X_2": pd.Series(x_a[:, 1]),
+                                "Class": pd.Series(["1" for _ in range(st.session_state.class_A_size)])} )
 
-        # if configure_saved:
-            # st.session_state.raw_data = data
+            ## Initialize or set Class B configurations
+            col2.write("Class B Configurations")
+            st.session_state.class_B_size = col2.number_input(
+                'Classs B size', 
+                value = st.session_state.class_B_size if 'class_B_size' in st.session_state else 1000)
+            st.session_state.class_B_theta_size = col2.number_input(
+                'Class B Spiral Half Cycle', 
+                value = st.session_state.class_B_theta_size if 'class_B_theta_size' in st.session_state else 2)
+            st.session_state.class_B_noise = col2.number_input(
+                'Class B, SD', 
+                value = st.session_state.class_B_noise if 'class_B_noise' in st.session_state else 1)
+
+            theta_2 = np.sqrt(np.random.rand(st.session_state.class_B_size))* st.session_state.class_B_theta_size * pi # np.linspace(0,2*pi,100)
+
+            r_b = -2*theta_2 - pi
+            data_b = np.array([np.cos(theta_2)*r_b, np.sin(theta_2)*r_b]).T
+            x_b = data_b + st.session_state.class_B_noise * np.random.randn(st.session_state.class_B_size,2)
+
+            df2 = pd.DataFrame({"X_1": pd.Series(x_b[:, 0]),
+                                "X_2": pd.Series(x_b[:, 1]),
+                                "Class": pd.Series(["0" for _ in range(st.session_state.class_B_size)])} )
+
+            st.session_state.raw_data = pd.concat([df1, df2]) #df1.append(df2, ignore_index = True)
+
+            fig = px.scatter(st.session_state.raw_data, x="X_1", y="X_2", color="Class", width=600, height=600)
+            fig.add_trace(
+                go.Scatter(
+                    name="axis",
+                    x=[0, 0, None, min(st.session_state.raw_data["X_1"]) , max(st.session_state.raw_data["X_1"]) ],
+                    y=[min(st.session_state.raw_data["X_2"]) , max(st.session_state.raw_data["X_2"]),  None, 0, 0],
+                    mode="lines",
+                    line=go.scatter.Line(color="purple"),
+                    showlegend=False)
+                )
+
+            col3.plotly_chart(fig, use_container_width=False)
 
 
     elif mode == "Classifier":
         st.header("Train and test the data")
 
-        if 'raw_data' not in st.session_state:
-            st.write("save raw data config first")
-
-        train_size = st.slider("Pick Training Size", min_value=0.0, max_value=1.0, value=0.7)
+        train_size = st.slider("Pick Training Size", min_value=0.0, max_value=1.0, value=st.session_state.train_size if 'train_size' in st.session_state else 0.7)
 
         ## preprocess data
         permuted_data = st.session_state.raw_data.sample(frac = 1, random_state = 100).reset_index(drop = True)
         
         X_train, X_test, y_train, y_test = train_test_split(permuted_data.drop('Class',axis=1), permuted_data["Class"], test_size = 1-train_size, random_state = 100) 
 
-        classifier_name = st.selectbox("Choose Type of Classifier", ["Dumb Classifier", "Logistic Regression", "SVM", "Neural Network"])
+        classifier_name = st.selectbox("Choose Type of Classifier", ["Dumb Classifier", "Logistic Regression", "KNN Classifier", "SVM", "Neural Network", "KNN Handcraft"])
         
         if classifier_name == "Dumb Classifier":
             y_pred = dumbClassifer(X_test)
         elif classifier_name == "Logistic Regression":
-            # recommended_weights = class_weight.compute_class_weight(class_weights = 'balanced',
-            #                                                         classes = np.unique(y_train),
-            #                                                         y = y_train)
-            # class_0_weight = st.slider("class weight on fit class 0", min_value=0.0, max_value=1.0, value=recommended_weights['0'])
-
-            # class_weight_lgr = {"0": class_0_weight,
-            #                     "1": class_0_weight * }
-
             logmodel = LogisticRegression(random_state=100).fit(X_train, y_train)
             y_pred = logmodel.predict_proba(X_test)[:,1]
+        elif classifier_name == "KNN Classifier":
+            neigh = KNeighborsClassifier(n_neighbors=6)
+            neigh.fit(X_train, y_train)
+            y_pred = neigh.predict_proba(X_test)[:,1]
+        elif classifier_name == "KNN Handcraft":
+            knn_classifier = knn.KNearestNeighbor()
+            knn_classifier.train(X_train.to_numpy(), y_train.to_numpy())
+            dists = knn_classifier.compute_distances_no_loops(X_test.to_numpy())
+            y_pred = knn_classifier.predict_labels(dists, k=8)
+            
 
         model_saved = st.button('Save Model?')
 
@@ -102,29 +195,28 @@ def main():
 
     elif mode == "Metrics":
         st.header("Metrics")
-        decision_threshold = st.slider("Threshold: ", 0.0, 1.0, 0.5)
-
-        obj = binary_classifier_tool(st.session_state.y_pred, st.session_state.y_test, step_size = 0.01)
-        obj.metrics_precompute()
-        st.plotly_chart(obj.plot_all_in_one_graph(threshold = decision_threshold), use_container_width=False)
-        st.plotly_chart(obj.plot_ROC(threshold = decision_threshold), use_container_width=False)
-        st.plotly_chart(obj.plot_PR_Curve(threshold = decision_threshold), use_container_width=False)
-
-
-        TP_num, FP_num, TN_num, FN_num, precision, recall, FPR, ABC, w_l, w_r = obj.get_CM_metrics(threshold=decision_threshold)
-        st.write("#TP = ", TP_num)
-        st.write("#FP = ", FP_num)
-        st.write("#TN = ", TN_num)
-        st.write("#FN = ", FN_num)
         
-        st.write("weight from left = ", w_l)
-        st.write("weight from right = ", w_r)
-        st.write("precision = ", precision)
-        st.write("recall = ", recall)
-        st.write("FPR = ", FPR)
-        st.write("Area Between Curves = ", ABC)
+        obj = binary_classifier_tool(st.session_state.y_pred, st.session_state.y_test, step_size = 0.001)
+        obj.metrics_precompute()
+        col1, col2 = st.columns(2)
 
-        st.plotly_chart(obj.plot_histogram())
+        decision_threshold = col1.slider("Threshold: ", 0.0, 1.0, 0.5)
+        col1.plotly_chart(obj.plot_all_in_one_graph(threshold = decision_threshold), use_container_width=True)
+        
+        col2.plotly_chart(obj.plot_ROC(threshold = decision_threshold), use_container_width=False)
+        col2.plotly_chart(obj.plot_PR_Curve(threshold = decision_threshold), use_container_width=False)
+
+
+        col3, col4 = st.columns(2)
+        TP_num, FP_num, TN_num, FN_num, precision, recall, FPR, ABC, w_l, w_r = obj.get_CM_metrics(threshold=decision_threshold)
+
+        metrics_df = pd.DataFrame({'Metrics':['Precision', 'Recall', 'FPR', 'Area Between Curves', "#TP", "#FP", "#TN", "#FN"],
+                                   'Value':[precision, recall, FPR, ABC, int(TP_num), int(FP_num), int(TN_num), int(FN_num)]}) 
+
+        col1.dataframe(metrics_df)
+
+
+        #st.plotly_chart(obj.plot_histogram())
 
 
 ## Dumb Classifier
--- libraries/binary_classifier_tool.py
+++ libraries/binary_classifier_tool.py
@@ -4,12 +4,13 @@ import math
 
 import plotly.express as px
 import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 
 from sklearn.metrics import roc_curve
 from sklearn.metrics import precision_recall_curve
 
 class binary_classifier_tool:
-    def __init__(self, y_pred, y_ground_truth, step_size = 0.1):
+    def __init__(self, y_pred, y_ground_truth, step_size = 0.0001):
         self.y_pred = y_pred
         self.y_ground_truth = y_ground_truth
         self.step_size = step_size
@@ -27,8 +28,8 @@ class binary_classifier_tool:
         ## Get the class_0_ratio cdf from bottom
         for upper_bound in bin_bounds:
 
-            num_class_0_bin = self.auxilary_df[(self.auxilary_df["y_ground_truth"] == "0") & (self.auxilary_df["y_pred"] < upper_bound)].shape[0]
-            total_num_bin = self.auxilary_df[(self.auxilary_df["y_pred"] < upper_bound)].shape[0]
+            num_class_0_bin = self.auxilary_df[(self.auxilary_df["y_ground_truth"] == "0") & (self.auxilary_df["y_pred"] <= upper_bound)].shape[0]
+            total_num_bin = self.auxilary_df[(self.auxilary_df["y_pred"] <= upper_bound)].shape[0]
 
             if total_num_bin == 0:
                 ratio_class_0_bin = 1
@@ -39,8 +40,8 @@ class binary_classifier_tool:
         ## Get the class_0_ratio cdf greater than threshold value
         for lower_bound in bin_bounds:
 
-            num_class_0_bin = self.auxilary_df[(self.auxilary_df["y_ground_truth"] == "0") & (self.auxilary_df["y_pred"] > lower_bound)].shape[0]
-            total_num_bin = self.auxilary_df[(self.auxilary_df["y_pred"] > lower_bound)].shape[0]
+            num_class_0_bin = self.auxilary_df[(self.auxilary_df["y_ground_truth"] == "0") & (self.auxilary_df["y_pred"] >= lower_bound)].shape[0]
+            total_num_bin = self.auxilary_df[(self.auxilary_df["y_pred"] >= lower_bound)].shape[0]
 
             if total_num_bin == 0:
                 ratio_class_0_bin = 0
@@ -48,10 +49,53 @@ class binary_classifier_tool:
                 ratio_class_0_bin = num_class_0_bin / total_num_bin 
             self.class_0_cdf_from_top.append(ratio_class_0_bin)
 
+        ## get how many counts in each bin
+        each_bin_count = []
+        lower = 0
+        for _ in range(50):
+            upper = lower + 0.02
+            coun = len(self.y_pred[(self.y_pred >= lower) & (self.y_pred <= upper)])
+            each_bin_count.append(coun)
+            lower += 0.02
+        self.hist_threshold_ceiling = max(each_bin_count)
+
 
     def plot_all_in_one_graph(self, threshold = 0.5):
+        fig = make_subplots(rows=2, cols=1,
+                    row_heights=[0.2, 0.8],
+                    vertical_spacing = 0.02,
+                    shared_yaxes=False,
+                    shared_xaxes=True)
+
         cdf_cutoff = math.floor(threshold/self.step_size)
-        fig = px.line(y=self.class_0_cdf_from_bottom[:cdf_cutoff], x = np.arange(0, threshold, self.step_size),  width=600, height=600)
+        
+        ## add shaded shape
+        fig.add_shape(type="rect", x0=threshold, y0=self.class_0_cdf_from_top[cdf_cutoff], x1=1, y1=1, line=dict(color="RoyalBlue", width=2,), fillcolor="#94D9FF", layer="below", row = 2, col = 1)
+        fig.add_shape(type="rect", x0=0, y0=0, x1=threshold, y1=self.class_0_cdf_from_bottom[cdf_cutoff], line=dict(color="RoyalBlue", width=2,), fillcolor="#F77559", layer="below", row = 2, col = 1)
+        
+        ## add label annotations
+        fig.add_annotation(x=(1 + threshold)/2, y=(1+self.class_0_cdf_from_top[cdf_cutoff])/2, text="TP", showarrow=False, arrowhead=2, row = 2, col = 1)
+        fig.add_annotation(x=threshold/2, y=self.class_0_cdf_from_bottom[cdf_cutoff]/2, text="TN", showarrow=False, arrowhead=2, row = 2, col = 1)
+        fig.add_annotation(x=threshold/2, y=(1+self.class_0_cdf_from_bottom[cdf_cutoff])/2, text="FN", showarrow=False, arrowhead=2, row = 2, col = 1)
+        fig.add_annotation(x=(1 + threshold)/2, y=self.class_0_cdf_from_top[cdf_cutoff]/2,text="FP", showarrow=False, arrowhead=2, row = 2, col = 1)
+
+
+        fig.add_trace(go.Histogram(x=self.auxilary_df[self.auxilary_df["y_ground_truth"] == "1"]['y_pred'], xbins=dict(size=0.02), name="class1"), row = 1, col = 1)
+        fig.add_trace(go.Histogram(x=self.auxilary_df[self.auxilary_df["y_ground_truth"] == "0"]['y_pred'], xbins=dict(size=0.02), name="class0"), row = 1, col = 1)
+        fig.add_trace(go.Scatter(name="Threshold", x=[threshold, threshold], y=[-20, self.hist_threshold_ceiling], mode="lines", line=go.scatter.Line(color="red"), showlegend=False), row = 1, col = 1)
+
+
+        fig.add_trace(
+            go.Scatter(
+                name="trend_from_lower",
+                x=np.arange(0, threshold, self.step_size),
+                y=self.class_0_cdf_from_bottom[:cdf_cutoff],
+                mode="lines",
+                line={'color': 'blue'},
+                showlegend=False),
+                row = 2, col = 1
+            )   
+        #fig = px.line(y=self.class_0_cdf_from_bottom[:cdf_cutoff], x = np.arange(0, threshold, self.step_size),  width=400, height=400)
         fig.add_trace(
             go.Scatter(
                 name="trend_from_lower",
@@ -59,7 +103,8 @@ class binary_classifier_tool:
                 y=self.class_0_cdf_from_bottom[cdf_cutoff:],
                 mode="lines",
                 line={'color': 'grey'},
-                showlegend=False)
+                showlegend=False),
+                row = 2, col = 1
             )
         fig.add_trace(
             go.Scatter(
@@ -68,7 +113,8 @@ class binary_classifier_tool:
                 y=self.class_0_cdf_from_top[cdf_cutoff: ],
                 mode="lines",
                 line={'color': 'blue'},
-                showlegend=False)
+                showlegend=False),
+                row = 2, col = 1
             )
         fig.add_trace(
             go.Scatter(
@@ -77,38 +123,42 @@ class binary_classifier_tool:
                 y=self.class_0_cdf_from_top[:cdf_cutoff],
                 mode="lines",
                 line={'color': 'grey'},
-                showlegend=False)
+                showlegend=False),
+                row = 2, col = 1
             )
         
         fig.add_trace(
             go.Scatter(
                 name="Boundaries",
-                x=[1, 1, None, -0.2, 1.2, None, 0, 0, None, 0, 1],
-                y=[0, 1, None, 0, 0, None, -0.2, 1.2, None, 1, 1],
+                x=[1, 1, None, -0.1, 1.1, None, 0, 0, None, 0, 1],
+                y=[0, 1, None, 0, 0, None, -0.15, 1.15, None, 1, 1],
                 mode="lines",
                 line=go.scatter.Line(color="purple"),
-                showlegend=False)
+                showlegend=False),
+                row = 2, col = 1
             )
 
         fig.add_trace(
             go.Scatter(
                 name="Threshold",
                 x=[threshold, threshold],
-                y=[-0.2, 1.2],
+                y=[-0.1, 1.1],
                 mode="lines",
                 line=go.scatter.Line(color="red"),
-                showlegend=False)
+                showlegend=False),
+                row = 2, col = 1
             )
 
         baseline = sum(self.y_ground_truth == "0")/len(self.y_ground_truth)
         fig.add_trace(
             go.Scatter(
                 name="Baseline",
-                x=[-0.2, 1.2],
+                x=[-0.05, 1.05],
                 y=[baseline, baseline],
                 mode="lines",
                 line={'dash': 'dot', 'color': 'green'},
-                showlegend=False)
+                showlegend=False),
+                row = 2, col = 1
             )
         
         fig.add_trace(
@@ -118,15 +168,27 @@ class binary_classifier_tool:
                 y=[self.class_0_cdf_from_bottom[cdf_cutoff], self.class_0_cdf_from_bottom[cdf_cutoff], None, self.class_0_cdf_from_top[cdf_cutoff], self.class_0_cdf_from_top[cdf_cutoff]],
                 mode="lines",
                 line={'dash': 'dash', 'color': 'orange'},
-                showlegend=False)
+                showlegend=False),
+                row = 2, col = 1
             )
 
+        
+        fig.update_layout(
+                bargap=0,
+                bargroupgap = 0,
+                width=600,
+                height=650,
+                legend=dict(orientation="v", yanchor="top", y = 0.98, xanchor="left", x= 0.99, font=dict(family="Courier",size=8,color="black"))
+            )
+
+        fig.update_xaxes(title_text='y_pred', row = 2, col = 1)
+        fig.update_yaxes(title_text='Class 0 Proportion', row = 2, col = 1)
         return fig
 
     def plot_ROC(self, threshold):
         fpr, tpr, _ = roc_curve(np.array(self.y_ground_truth == '1'), self.y_pred)
 
-        fig1 = px.line(y=tpr, x = fpr, width=600, height=600)
+        fig1 = px.line(y=tpr, x = fpr)
         fig1.add_trace(
             go.Scatter(
                 x=[0,1],
@@ -141,19 +203,27 @@ class binary_classifier_tool:
         fig2 = go.Figure(data=go.Scatter(
             x=[FPR],
             y=[recall_TPR], 
+            name="Cutoff",
+            mode = "markers",
             marker=dict(color=[2])
         ))
-        fig2.update_layout(
-                width=600,
-                height=600
-                )
         
         fig3 = go.Figure(data=fig1.data + fig2.data)
+
+        fig3.update_layout(
+                title={
+                        'text': "ROC Curve",
+                        'yanchor': 'top'},
+                width=400,
+                height=400
+                )
+        fig3.update_xaxes(title_text='False Positive Rate')
+        fig3.update_yaxes(title_text='True Positive Rate(Recall)')
         return fig3
 
     def plot_PR_Curve(self, threshold):
         precision, recall, _ = precision_recall_curve(np.array(self.y_ground_truth == '1'), self.y_pred)
-        fig1 = px.line(y=precision, x = recall, width=600, height=600)
+        fig1 = px.line(y=precision, x = recall, title="PR curve")
         baseline = sum(self.y_ground_truth == "1")/len(self.y_ground_truth)
         fig1.add_trace(
             go.Scatter(
@@ -169,14 +239,21 @@ class binary_classifier_tool:
         fig2 = go.Figure(data=go.Scatter(
             x=[recall_TPR],
             y=[Precision], 
-            marker=dict(color=[2])
+            name="Cutoff",
+            mode = "markers",
+            marker=dict(color=[2], size = 8)
         ))
-        fig2.update_layout(
-                width=600,
-                height=600
-                )
-        
+
         fig3 = go.Figure(data=fig1.data + fig2.data)
+        
+        fig3.update_layout(
+                title={
+                    'text': "PR Curve",
+                    'yanchor': 'top'},
+                width=400,
+                height=400)
+        fig3.update_xaxes(title_text='Recall')
+        fig3.update_yaxes(title_text='Precision')
         return fig3
 
 
@@ -190,9 +267,9 @@ class binary_classifier_tool:
         TN_num = self.auxilary_df[(self.auxilary_df["y_ground_truth"] == "0") & (self.auxilary_df["y_pred"] <= threshold)].shape[0]
         FN_num = self.auxilary_df[(self.auxilary_df["y_ground_truth"] == "1") & (self.auxilary_df["y_pred"] <= threshold)].shape[0]
 
-        precision = TP_num/(TP_num + FP_num)
-        recall_TPR = TP_num/(TP_num + FN_num)
-        FPR = FP_num/(FP_num + TN_num)
+        precision = TP_num/(TP_num + FP_num) if (TP_num + FP_num) > 0  else 1
+        recall_TPR = TP_num/(TP_num + FN_num) if (TP_num + FN_num) > 0 else 1
+        FPR = FP_num/(FP_num + TN_num) if (FP_num + TN_num) > 0 else 0
 
         Area_between_curve = np.sum(np.subtract(self.class_0_cdf_from_bottom, self.class_0_cdf_from_top) * self.step_size )
 
@@ -200,5 +277,3 @@ class binary_classifier_tool:
         weight_from_right = 1 - weight_from_left
 
         return TP_num, FP_num, TN_num, FN_num, precision, recall_TPR, FPR, Area_between_curve, weight_from_left, weight_from_right
-
-
